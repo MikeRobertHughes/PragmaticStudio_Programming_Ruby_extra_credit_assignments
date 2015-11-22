b237a933e908c51380b7b308d9d582ac599b03ef
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-11-21 18:07:28 -0600

    Create an array of projects and iterate

--- crowdfund.rb
+++ crowdfund.rb
@@ -18,12 +18,12 @@ class Project
 
   def lose_funds(total_lost)
     @total -= total_lost
-    "Proj #{@name} lost some funds!"
+    "Proj #{@name} lost $#{total_lost} in funds!"
   end
 
   def gain_funds(total_gained)
     @total += total_gained
-    "Proj #{@name} got more funds!"
+    "Proj #{@name} gained $#{total_gained} in funds!"
   end
 
   def funding_needed
@@ -40,3 +40,16 @@ puts proj1.lose_funds(200)
 puts proj1
 puts proj1.gain_funds(700)
 puts proj1
+
+projects = [proj1, proj2, proj3]
+puts projects
+
+puts "There are #{projects.size} projects that need to be crowdfunded."
+
+projects.each do |project|
+  puts project
+  puts "Project #{project.name} has a goal of $#{project.goal}."
+  puts project.lose_funds(10)
+  puts project.gain_funds(30)
+  puts "Project #{project.name} still needs $#{project.funding_needed}."
+end
