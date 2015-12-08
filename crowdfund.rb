fee51e8ccaa25aafd926ae236dad28e83e89940f
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-12-08 17:14:08 -0600

    Allow user to choose number of rounds of funding

--- crowdfund.rb
+++ crowdfund.rb
@@ -7,6 +7,17 @@ nonprofits = FundRequest.new("Nonprofit Projects")
 nonprofits.add_project(proj1)
 nonprofits.add_project(proj2)
 nonprofits.add_project(proj3)
-nonprofits.report(3)
 
-nonprofits.print_stats
+loop do
+  puts "\nHow many rounds of funding would you like? ('quit' to exit)"
+  answer = gets.chomp.downcase
+  case answer
+  when /^\d+$/
+    nonprofits.report(answer.to_i)
+  when 'quit', 'exit'
+    nonprofits.print_stats
+    break
+  else
+    puts "Please enter a number or 'quit'"
+  end
+end
